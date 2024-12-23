19171000f2691c2cd543b62bf4fd7abc30e52459
jhonatann-per <jhonatanpereira152@gmail.com>
2024-12-22 23:37:10 -0300

    Adiciona a lista de qualidades, pega as informações de localização e LinkedIn do GitHub, e o slide de front/back-end e estilizações dos mesmos

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,39 +1,92 @@
 import React, { useState, useEffect } from "react";
-import { 
-  Container, Apresentation, 
+import { FaMapMarkerAlt, FaLinkedin } from 'react-icons/fa';
+import {
+  Container,
   ImagePerfil,
-  Titulo, Texto, 
-  ConteudoTitulo,
-  ConteudoTexto
+  Texto,
+  ConteudoPerfil,
+  ConteudoTexto,
+  TituloContainer,
+  ListDiv,
+  List,
+  ListItem,
+  IconsDiv,
+  Skils,
+  Titulo
 } from "./styles";
 
 const AboutMe = () => {
-  const [perfilImg, setPerfilImg] = useState('')
+  const [perfilImg, setPerfilImg] = useState('');
+  const [githubData, setGithubData] = useState({});
+  const [currentWord, setCurrentWord] = useState('Desenvolvedor');
 
-  useEffect(() =>{
-    fetch('https://api.github.com/users/jhonatann-per')
-      .then(res => res.json())
-      .then(data => setPerfilImg(data.avatar_url))
-      .catch(error => console.error("Erro: imagem não encontrada!"))
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      try {
+        const res = await fetch('https://api.github.com/users/jhonatann-per');
+        const data = await res.json();
+        setPerfilImg(data.avatar_url);
+        setGithubData(data);
+      } catch (error) {
+        console.error("Erro: dados não encontrados!", error);
+      }
+    };
+    fetchProfileData();
+  }, []);
+
+  useEffect(() => {
+    const words = ['front-end', 'back-end'];
+    let index = 0;
+
+    const intervalId = setInterval(() => {
+      index = (index + 1) % words.length;
+      setCurrentWord(words[index]);
+    }, 4500);
+
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
-    <Container id="sobreMim">
-          <ConteudoTitulo>
-            <Titulo>Jhonatan Santos</Titulo>
-            {perfilImg && <ImagePerfil 
-              src={perfilImg} 
-              alt="Imagem de perfil" 
-            />}
-          </ConteudoTitulo>
-            
-          <ConteudoTexto>
-            <Texto>
-              Desde 2019, atuo na área de tecnologia, focando em desenvolver minhas habilidades e conhecimentos. 
-              Atualmente, estou buscando realizar meu sonho de me tornar um desenvolvedor fullstack, combinando 
-              minha paixão por front-end e back-end para criar soluções completas e inovadoras.
-            </Texto>
-          </ConteudoTexto>
+    <Container>
+      <ConteudoPerfil>
+        <TituloContainer>
+          <Titulo>Jhonatan Santos</Titulo>
+        </TituloContainer>
+        { perfilImg && (
+          <ImagePerfil 
+            src={perfilImg} 
+            alt="Imagem de perfil" 
+          />
+        )}
+        { githubData && (
+          <IconsDiv>
+            <p><FaMapMarkerAlt style={{ marginRight: '8px', color: '#fff' }}/>  {githubData.location}</p>
+            <p><FaLinkedin style={{ marginRight: '8px', color: '#0077b5' }} /> <a href="https://www.linkedin.com/in/jhonatann-per/" target="_blank" rel="noopener noreferrer">Meu LinkedIn</a></p>
+          </IconsDiv>
+        )}
+        <Skils>
+          <Titulo>{currentWord}</Titulo>
+        </Skils>
+      </ConteudoPerfil>
+      
+      <ConteudoTexto>
+        <Texto>
+          Desde 2019, atuo na área de tecnologia, focando em desenvolver minhas habilidades e conhecimentos. 
+          Atualmente, estou buscando realizar meu sonho de me tornar um desenvolvedor fullstack, combinando 
+          minha paixão por front-end e back-end para criar soluções completas e inovadoras.
+        </Texto>
+        
+        <ListDiv>
+          <List>
+            <ListItem style={{ fontSize: '1.5em', fontWeight: 'bold', content: "none" }}>Qualidades: </ListItem>
+            <ListItem>⭐Proativo</ListItem>
+            <ListItem>⭐Trabalho em equipe</ListItem>
+            <ListItem>⭐Resiliente</ListItem>
+            <ListItem>⭐Comunicativo</ListItem>
+            <ListItem>⭐Aprendizado rápido</ListItem>
+          </List>
+        </ListDiv>
+      </ConteudoTexto>
     </Container>
   );
 };
--- src/components/AboutMe/styles.js
+++ src/components/AboutMe/styles.js
@@ -1,42 +1,115 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const slideAndFade = keyframes`
+  0% {
+    transform: translateX(100%);
+    opacity: 0;
+  }
+  50% {
+    transform: translateX(0);
+    opacity: 1;
+  }
+  100% {
+    transform: translateX(-100%);
+    opacity: 0;
+  }
+`;
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: row;
+  justify-content: space-between;
   align-items: center;
   width: 80vw;
   padding: 20px;
-  background-color: #f0f0f0;
   border-radius: 10px;
   margin: 10px 0;
+  height: 70vh;
 `;
 
-
-export const ImagePerfil = styled.img`
-  width: 250px;
-  height: 250px;
-  border-radius: 50%;
-`;
-
-export const Titulo = styled.h2`
-  margin-bottom: 10px;
-  color: #333;
-`;
-
-export const Texto = styled.p`
-  color: #666;
-  margin-bottom: 15px;
-`;
-
-export const ConteudoTitulo = styled.div`
+export const ConteudoPerfil = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;;
+  align-items: center;
   width: 25%;
   margin-bottom: 10px;
 `;
 
+export const TituloContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+export const Titulo = styled.h2`
+  margin-bottom: 10px;
+  color: #fff;
+`;
+
+export const ImagePerfil = styled.img`
+  width: 80%;
+  height: auto;
+  border-radius: 50%;
+  box-shadow: 0 0 20px rgba(255, 255, 255, 0.6);
+`;
+
 export const ConteudoTexto = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 60%;
-  margin-left:10px;
+  margin-left: 10px;
+  text-align: justify;
+`;
+
+export const Texto = styled.p`
+  color: white;
+  margin-bottom: 15px;
+  font-size: 20px;
+  font-weight: normal;
+`;
+
+export const ListDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding-top: 15px;
+  text-align: left;
+`;
+
+export const List = styled.ul`
+  color: white;
+  list-style: none;
+`;
+
+export const ListItem = styled.li`
+  color: white;
+  padding: 5px;
+  font-size: 18px;
+  font-weight: normal;
+  display: inline;
+`;
+
+export const IconsDiv = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 15px;
+  color: white;
+
+  p {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 15px;
+
+    a {
+      color: white;
+      text-decoration: none;
+    }
+  }
+`;
+
+export const Skils = styled.div`
+  animation: ${slideAndFade} 4.5s linear infinite;
 `;
